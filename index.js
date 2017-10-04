336283a982f915e05b4e9898a6d4f6955c059121
Sunit Kulkarni <sunitkulkarni@Sunits-MacBook-Pro-2.local>
2017-10-03 21:10:29 -0400

    working movie searcher

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /*global $ */
+/*jslint vars: true*/
+/**
+* @returns {HTMLDivElement}
+*/
 
 $(function () {
     //listen for when the form submits
@@ -10,20 +14,39 @@ $(function () {
     
     "use strict";
     
+    //rendering function for displaying movies to the screen
+    function renderMovies(movies) {
+        $(".card-columns").empty(); //clears current results
+            
+        movies.Search.forEach(function (movie) {
+            
+            //Variable Declarations for Card DOM Elements
+            var newCardDiv = $("<div class='card' style='width: 20rem;'></div>");
+            var imagePoster = $('<img class="card-img-top"/>');
+            var newCardBodyDiv = $("<div class='card-body'></div>");
+            var newCardTitle = $("<h4 class='card-title'>" + movie.Title + "</h4>");
+            var newCardBadgeYear = $('<span class="badge badge-secondary float-right">' + movie.Year + '</span>');
+            var newParagraph = $("<p class='card-text'>Quick Example Text</p>");
+            var newAnchor = $("<a href='#' class='btn btn-primary'>Add</a>");
+            
+            imagePoster.attr('src', movie.Poster); //inserting movie Poster
+            newCardTitle.append(newCardBadgeYear); //inserting movie Year
+            newCardBodyDiv.append(newCardTitle, newParagraph, newAnchor);
+            
+            newCardDiv.append(imagePoster, newCardBodyDiv);
+            $(".card-columns").append(newCardDiv); //adds movie card to screen
+        });
+    }
+    
     $("form").submit(function (event) {
         event.preventDefault(); //stops a page refresh
         
-        var searchString = $('.searchBar').val();
-        var url = 'http://www.omdbapi.com/?apikey=3430a78&s=' + searchString;
+        var searchString = $('.searchBar').val(),
+            url = 'http://www.omdbapi.com/?apikey=3430a78&s=' + searchString;
         
-        $.get(url, function (data) {
-            $(".card-columns").empty(); //clears current results
+        $.get(url, function (movieDatabase) {
+            renderMovies(movieDatabase);
             
-            data.Search.forEach(function (movie) {
-                
-                var newDiv = $("<div></div>");
-                newDiv.append("<h1>hello</h1>");
-            });
         });
         
     });
